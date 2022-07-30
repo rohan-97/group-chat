23f9ee2d395a8d3357e367ddb2e73443dc4fb40c
Rohan <yrohan31@gmail.com>
2022-07-30 22:07:59 +0530

    documenting admin panel page

--- chat_app/view/admin.py
+++ chat_app/view/admin.py
@@ -3,17 +3,9 @@ from flask_admin.contrib.sqla import ModelView
 from chat_app.model.data import *
 from chat_app import app
 """
-class GroupMembersModelView(ModelView):
-    column_list = ['gid', 'uid', 'is_group_admin']
-
-class UserModelView(ModelView):
-    column_list = ['uid', 'username', 'password', 'name', 'is_admin']
-
-class GroupModelView(ModelView):
-    column_list = ['gid', 'name', 'description']
-
-class MessageLikeMapModelView(ModelView):
-    column_list = ['msgid', 'uid']
+For addition of user and user modification, we are leveraging flask-admin panel
+This panel allows us to add user with specific attributes and presents a UI for the same.
+we are just linking the admin UI to main page dashboard
 """
 admin = Admin(app)
 admin.add_view(ModelView(User, DB.session))
